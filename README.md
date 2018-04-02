04e7e68a832bdf4699d72882c0c01966db8c1691
yatou666 <1904692862@qq.com>
2018-04-03 00:14:39 +0800

    1

--- README.md
+++
@@ -1,1 +0,0 @@
-# tianmao
--- js/index.js
+++ js/index.js
@@ -1,47 +1,63 @@
   {
     //banner部分
     $(".pgct li").each(function (index,ele) {
-        $(this).click(function () {
+        $(this).mouseover(function () {
             $(this).addClass("active").siblings(".active")
                 .removeClass("active")
             $(".imgbox li").removeClass("active")
                 .eq(index)
                 .addClass("active")
+            // $(".leftbox").css("background","green");
         })
     })
 }
  
 
- $("#totop").click(function () {
+$("#db").click(function () {
 
-            $("html,body").animate({scrollTop:0},500)
-        })
+    $("html,body").animate({scrollTop:0},500)
+})
 
 
 
-let leftBar=document.querySelector(".leftBar");
-let topBar=document.querySelector(".topBar");
-window.onscroll=function(){
-        let st=document.documentElement.scrollTop;
 
-        console.log(st);
-        if(st>700){
-            leftBar.style.display="block";
-            topBar.style.display="block";
-        }else{
-            leftBar.style.display="none";
-             topBar.style.display="none";
-        }
+ window.onscroll=function(){
+    let ot=$("html,body").scrollTop();
+    if(ot>700){
+       $(".leftBar").css("display","block"); 
+       $(".topBar").css("display","block"); 
+    }
+    else{
+       $(".leftBar").css("display","none"); 
+       $(".topBar").css("display","none"); 
+    }
+ }
 
-}
+// let leftBar=document.querySelector(".leftBar");
+// let topBar=document.querySelector(".topBar");
+// window.onscroll=function(){
+//         let st=document.documentElement.scrollTop;
 
-console.log($(".content"));
+//         console.log(st);
+//         if(st>700){
+//             leftBar.style.display="block";
+//             topBar.style.display="block";
+//         }else{
+//             leftBar.style.display="none";
+//              topBar.style.display="none";
+//         }
+
+// }
+
+
 
 
 $(".tips").click(function () {
-    alert(789);
+
     let index=$(this).index(".tips");
+    console.log(index);
     let ot=$(".tiaolou").eq(index).offset().top-665;
+    console.log(ot);
     $("html,body").animate({scrollTop:ot},200)
 })
 
@@ -50,12 +66,44 @@ $(window).scroll(function () {
     $(".tiaolou").each(function (index,ele) {
         if(st>$(this).offset().top-665){
             $(".tips").removeClass("active")
-                .eq(index).addClass("active");
+                .eq(index+1).addClass("active");
         }
     })
 })
 
 
+ //右侧导航栏部分
+{
+    $(".biaoqian li").each(function (index,ele) {
+
+
+        $(ele).mouseover(function () {
+
+
+            $(this).addClass("righthide").siblings(".righthide")
+                .removeClass("righthide");
+            $(".righthide div")
+                .addClass("hide1").siblings(".hide1")
+                .removeClass("hide1")
+        })
+    })
+}
+
+{
+      $(".biaoqian2 li").each(function (index,ele) {
+
+
+          $(ele).mouseover(function () {
+
+
+              $(this).addClass("righthide").siblings(".righthide")
+                  .removeClass("righthide");
+              $(".righthide div")
+                  .addClass("hide3").siblings(".hide3")
+                  .removeClass("hide3")
+          })
+      })
+  }
 
 
 
