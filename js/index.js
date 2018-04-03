75de262187fd84704ea3dab0ad62410212c62b2e
yatou666 <1904692862@qq.com>
2018-04-03 15:01:56 +0800

    1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-  {
+{
     //banner部分
     $(".pgct li").each(function (index,ele) {
         $(this).mouseover(function () {
@@ -7,11 +7,31 @@
             $(".imgbox li").removeClass("active")
                 .eq(index)
                 .addClass("active")
-            // $(".leftbox").css("background","green");
         })
     })
 }
- 
+  let imgs=document.querySelectorAll(".imgbox li");
+  let pagers=document.querySelectorAll(".pgct li");
+  let banner=document.querySelector(".picture");
+  
+let n=0; 
+  let t=setInterval(move,3000);
+  function move(){
+    n++;
+    if(n===imgs.length){
+      n=0;
+    }
+    if(n<0){
+      n=imgs.length-1;
+    }
+    for(let i=0;i<imgs.length;i++){
+      imgs[i].classList.remove("active");
+      pagers[i].classList.remove("active");
+    }
+    imgs[n].classList.add("active");
+    pagers[n].classList.add("active");
+  }
+
 
 $("#db").click(function () {
 
@@ -56,7 +76,7 @@ $(".tips").click(function () {
 
     let index=$(this).index(".tips");
     console.log(index);
-    let ot=$(".tiaolou").eq(index).offset().top-665;
+    let ot=$(".tiaolou").eq(index).offset().top-50;
     console.log(ot);
     $("html,body").animate({scrollTop:ot},200)
 })
@@ -64,9 +84,9 @@ $(".tips").click(function () {
 $(window).scroll(function () {
     let st= $(window).scrollTop();
     $(".tiaolou").each(function (index,ele) {
-        if(st>$(this).offset().top-665){
+        if(st>$(this).offset().top-150){
             $(".tips").removeClass("active")
-                .eq(index+1).addClass("active");
+                .eq(index).addClass("active");
         }
     })
 })
